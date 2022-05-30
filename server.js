cc38dcc0ee10caa235e96cd5636bc8d943164b2a
kononova-daria <76452663+kononova-daria@users.noreply.github.com>
2022-05-31 02:38:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ router.get('/posts', async (ctx, next) => {
 });
 
 router.post('/posts', async(ctx, next) => {
-    const {id, content} = ctx.request.body;
+    const {id, text} = ctx.request.body;
 
     if (id !== 0) {
         posts = posts.map(o => o.id !== id ? o : {...o, text: text});
